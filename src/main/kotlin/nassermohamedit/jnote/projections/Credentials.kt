526e1acd10405815bbdf6faf8c98f9e3abce41aa
Nasser Mohamed <nassermohamedit@gmail.com>
2024-07-14 15:50:56 +0100

    Add RegisterForReflection for native image

--- src/main/kotlin/nassermohamedit/jnote/projections/Credentials.kt
+++ src/main/kotlin/nassermohamedit/jnote/projections/Credentials.kt
@@ -1,5 +1,7 @@
 package nassermohamedit.jnote.projections
 
+import io.quarkus.runtime.annotations.RegisterForReflection
 import nassermohamedit.jnote.entity.Role
 
+@RegisterForReflection
 data class Credentials(val id: Long, val password: String, val role: Role)
